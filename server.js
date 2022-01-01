82ca44119c6399fd971434e5924dacf2d547265b
santtier <santitodarello14@gmail.com>
2022-01-01 00:41:54 -0300

    Deployment of the project with heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.use(express.static('public'))
 
@@ -8,6 +9,6 @@ app.use(function (req, res) {
   res.sendFile(path.resolve(__dirname, 'index.html'))
 })
 
-app.listen(3000, _ => {
+app.listen(port, _ => {
   console.log('Listening on http://localhost:3000')
 })
